92439c959253cdc9cdc65ce3a06104f695d9c685
Administrator <administrator@G305-CC0077.local>
2024-10-29 17:37:54 +0800

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export const NavBar = ({ children }) => {
+  const router = useRouter();
+  const goHome = () => {
+    router.push("/");
+  };
   return (
     <div>
       <div className="icon">
-        <div
-          className="home"
-          // onClick={goHome}
-        >
+        <div className="home" onClick={goHome}>
           <img
             className="pic"
             src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
           />
           HOME
         </div>
-        <div className="createPost">
+        {/* <div className="createPost">
           <img
             className="pic"
             src="https://static.thenounproject.com/png/214735-200.png"
           />
           CREATE POST
-        </div>
+        </div> */}
       </div>
       {children}
     </div>
